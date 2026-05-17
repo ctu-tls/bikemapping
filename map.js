3117a8e796bf54a6acd528b8b3b85bbc7af8216f
Claire Tu <c7tu@ucsd.edu>
2026-05-16 23:31:53 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,4 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
-
-import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYzd0dSIsImEiOiJjbXA5ZTFxMDkwMHVhMnNvbXNxaG8zMTNzIn0.9AAaK0CvEDWzmPiES_08Gg';
